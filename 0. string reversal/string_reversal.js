0410f1740b5498bf9f017c24b5a521f9bfe26abc
Iftee1711402 <tawsifhaque97@gmail.com>
2022-06-13 21:58:07 +0600

    ...

--- 0. string reversal/string_reversal.js
+++ 0. string reversal/string_reversal.js
@@ -6,10 +6,18 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
+// solution:
 function reverse(str) {
   return str.split('').reverse().join('');
 }
-// console.log(reverse('Greetings!'));
-// console.log(reverse('olleh'));
-// console.log(reverse('leppa'));
+console.log(reverse('elppa'));
 
+// solution-2:
+function reverse1(str) {
+  let reversed = '';
+  for (let char of str) {
+    reversed = char + reversed;
+  }
+  return reversed;
+}
+console.log(reverse1('elppa'));
