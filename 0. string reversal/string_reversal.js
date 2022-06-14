bd3234f1cfe282d1724549d757d002d60464b962
Iftee1711402 <tawsifhaque97@gmail.com>
2022-06-14 21:47:02 +0600

    initial commit

---
+++ 0. string reversal/string_reversal.js
@@ -0,0 +1,14 @@
+// --- Directions
+// Given a string, return a new string with the reversed
+// order of characters
+// --- Examples
+//   reverse('apple') === 'elppa'
+//   reverse('hello') === 'olleh'
+//   reverse('Greetings!') === '!sgniteerG'
+
+function reverse(str) {
+  return str.split('').reverse().join('');
+}
+console.log(reverse('Greetings!'));
+console.log(reverse('olleh'));
+console.log(reverse('leppa'));
