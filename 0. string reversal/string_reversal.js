32f2ae600e202192cc71cc0e96f270e4a69f3118
Iftee1711402 <tawsifhaque97@gmail.com>
2022-06-21 17:17:36 +0600

    ...

--- 0. string reversal/string_reversal.js
+++ 0. string reversal/string_reversal.js
@@ -1,13 +1,14 @@
 // --- Directions
 // Given a string, return a new string with the reversed
 // order of characters
+
 // --- Examples
 //   reverse('apple') === 'elppa'
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
 
-// solution:
+// solution: the best solution
 function reverse(str) {
   return str.split('').reverse().join('');
 }
