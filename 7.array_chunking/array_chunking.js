589b240ff27c4d00eb2989746ebec8682989a37d
Iftee1711402 <tawsifhaque97@gmail.com>
2022-07-03 19:27:22 +0600

    ...

---
+++ 7.array_chunking/array_chunking.js
@@ -0,0 +1,27 @@
+// --- Directions
+// Given an array and chunk size, divide the array into many subarrays
+// where each subarray is of length size
+
+// --- Examples
+// chunk([1, 2, 3, 4], 2) --> [[ 1, 2], [3, 4]]
+// chunk([1, 2, 3, 4, 5], 2) --> [[ 1, 2], [3, 4], [5]]
+// chunk([1, 2, 3, 4, 5, 6, 7, 8], 3) --> [[ 1, 2, 3], [4, 5, 6], [7, 8]]
+// chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
+// chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
+
+
+// solution 1:
+function chunk(array, size) {
+  let chunked = [];
+  let index = 0;
+  while (index < array.length) {
+    chunked.push(array.slice(index, index + size));
+    index += size;
+  }
+  return chunked;
+}
+// console.log(chunk([1, 2, 3, 4], 2));
+// console.log(chunk([1, 2, 3, 4, 5], 2));
+// console.log(chunk([1, 2, 3, 4, 5, 6, 7, 8], 3));
+// console.log(chunk([1, 2, 3, 4, 5], 4));
+// console.log(chunk([1, 2, 3, 4, 5], 10));
